571f247fb9784e12a97d1da5c642f6341b4953b2
Nesar Ahmed <dev.nesar24@gmail.com>
2019-03-18 15:16:55 +0100

    initialize git

---
+++ src/test/java/calc/CalculatorTest.java
@@ -0,0 +1,42 @@
+package calc;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private static CalculatorMain calc;
+
+    @BeforeClass
+    public static void setup(){
+        calc = new CalculatorMain();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(3,2));
+    }
+    @Test
+    public void testSub() {
+        assertEquals(1, calc.sub(3,2));
+    }
+    @Test
+    public void testMul() {
+        assertEquals(6, calc.mul(3,2));
+    }
+    @Test
+    public void testDiv() {
+        assertEquals(1, calc.div(2,2));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivByZero() {
+        calc.div(3, 0);
+    }
+    @Test
+    public void testPresedence() {
+        assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
+    }
+}
